8a8899d28d6cba5c864e3dacce2d53f2e7bd8935
Shearlonil <shearlonil@gmail.com>
2024-10-12 14:22:26 +0100

    adding card skeleton

--- src/Components/CardSkeleton.jsx
+++ src/Components/CardSkeleton.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import Skeleton from "react-loading-skeleton";
-import { Link } from "react-router-dom";
 
 const CardSkeleton = () => {
   /*  refs: WITH REACT SUSPENSE
